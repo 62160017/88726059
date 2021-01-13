ddaec5f3ac98e66ded1ef17f215687f23e398d22
62160017 <62160017@go.buu.ac.th>
2021-01-13 21:26:30 +0700

    'hw4'

--- hw4/js/addsong.js
+++ hw4/js/addsong.js
@@ -1,28 +1,30 @@
+var songs = JSON.parse(localStorage.getItem("songs"))
+        if(songs == null){
+            localStorage.setItem("songs",songs)
+            songs = [];
+        }
 function addsong(){
+
     songs = localStorage.getItem('songs');
-    if(songs === null){
-        songs = [];
-        
-    }else{
-        songs = JSON.parse(songs);
-    }
-    data = [
-        namesong = document.getElementById('addsong').value,
-        artist = document.getElementById('artist').value,
-        lyrics = document.getElementById('lyrics').value
-    ];
-        songs.push(data);
-    
-    localStorage.setItem('songs',JSON.stringify(songs));
-    document.getElementById('addsong').value =''
-    document.getElementById('artist').value =''
-    document.getElementById('lyrics').value =''
+        if(songs == 'null'){
+            songs = [];
+        }else{
+            songs = JSON.parse(songs);
+        }
+        data = [
+            namesong = document.getElementById('addsong').value,
+            artist = document.getElementById('artist').value,
+            lyrics = document.getElementById('lyrics').value
+        ];
+            songs.push(data);
+        console.log(songs);
+        localStorage.setItem('songs',JSON.stringify(songs));
+
 
 }
 
-function Clearform(){
-document.getElementById('addsong').value =''
-document.getElementById('artist').value =''
-document.getElementById('lyrics').value =''
-}
-localStorage.clear();
+function clearform() {
+    document.getElementById('addsong').value = ''
+    document.getElementById('artist').value = ''
+    document.getElementById('lyrics').value = ''
+  }
